f1a813a0b77aa8bc0a54d202bd8c2c4e5e6b24ef
Endi Sukaj <endisukaj@gmail.com>
2026-01-31 23:25:51 +0100

    Start rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-use gemini_rust::Gemini;
-use gemini_rust::Message;
+use langrust::client::Model;
+use langrust::{GeminiApiModel, GeminiModel, Message, client::FunctionCall};
 use std::error::Error;
 use std::io::Write;
 use std::process::ExitCode;
 use std::{env, io};
 
-use crate::dispatcher::conversation_to_content_builder;
-use crate::dispatcher::{Conversation, run_agent};
-use crate::tools::get_filesystem_registry;
-
-mod dispatcher;
 mod tools;
 
-/// Basic content generation example - demonstrates the simplest usage of the Gemini API
+pub struct Conversation {
+    pub contents: Vec<Message>,
+}
+
+impl Conversation {
+    pub fn add_function_call_message(&mut self, function_call: &FunctionCall) {
+        self.contents
+            .push(Message::function_call(function_call.clone()));
+    }
+
+    pub fn add_message(&mut self, assistant_message: Message) {
+        self.contents.push(assistant_message);
+    }
+}
+
 #[tokio::main]
 async fn main() -> ExitCode {
     match do_main().await {
@@ -24,7 +33,7 @@ async fn main() -> ExitCode {
     }
 }
 
-fn get_input() -> Result<String, Box<dyn Error>> {
+fn get_input() -> Result<String, Box<dyn Error + Send + Sync>> {
     print!("> ");
     io::stdout().flush()?;
 
@@ -34,20 +43,24 @@ fn get_input() -> Result<String, Box<dyn Error>> {
     Ok(input)
 }
 
-async fn do_main() -> Result<(), Box<dyn std::error::Error>> {
+async fn do_main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // Get API key from environment variable
     let api_key = env::var("GEMINI_KEY").expect("GEMINI_KEY environment variable not set");
 
     // Create a Gemini client with default settings (Gemini 2.5 Flash)
-    let client = Gemini::new(api_key)?;
-    conversation_loop(client).await?;
+    let client = GeminiApiModel {
+        client: reqwest::Client::new(),
+        api_key: api_key,
+        model: GeminiModel::Gemini25Flash,
+    };
+    conversation_loop(&client).await?;
 
     Ok(())
 }
 
-async fn conversation_loop(client: Gemini) -> Result<(), Box<dyn Error>> {
+async fn conversation_loop(client: &GeminiApiModel) -> Result<(), Box<dyn Error + Send + Sync>> {
     let mut state = Conversation { contents: vec![] };
-    let tool_registry = get_filesystem_registry();
+    // let tool_registry = get_filesystem_registry();
     loop {
         let system_prompt = "You are a helpful coding assistant that has access to the file contents of the project the user is working on";
         let user_message_content = get_input()?;
@@ -57,18 +70,18 @@ async fn conversation_loop(client: Gemini) -> Result<(), Box<dyn Error>> {
         }
 
         let user_message = Message::user(user_message_content);
-        let mut conversation = conversation_to_content_builder(&client, &state);
-        conversation = conversation
-            .with_system_prompt(system_prompt)
-            .with_message(user_message.clone())
-            .with_tool(tool_registry.get_gemini_tool())
-            .with_function_calling_mode(gemini_rust::FunctionCallingMode::Auto);
-        let response = conversation.execute().await?;
+        _ = state.add_message(user_message);
 
-        println!("request sent");
+        let mut request_builder = client.new_request();
+        let request = request_builder
+            .with_system(system_prompt.to_string())
+            .with_messages(state.contents.clone());
 
-        state.contents.push(user_message.clone());
-        run_agent(&response, &mut state, client.clone(), &tool_registry).await;
+        let response = request.completion().await?;
+
+        println!("{}", response.completion);
+
+        _ = state.add_message(Message::model(response.completion));
     }
     Ok(())
 }
